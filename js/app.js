12a1a53065f81642ed2470151011876a6f89d846
asingh369 <amritpalsinghs369@gmail.com>
2019-12-01 12:41:37 -0800

    set food position randomly

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+/**
+ * Set global variables
+ */
+//  size of cube
+const side = 3;
+// size of snake cube
+const snake_side = side / 40;
+
+/**
+ * Page setup
+ */
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   75,
@@ -5,26 +16,73 @@ var camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.z = 5;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(3, 3, 3);
-var edges = new THREE.EdgesGeometry(geometry);
-var cube = new THREE.LineSegments(
-  edges,
-  new THREE.LineBasicMaterial({ color: 0xffffff })
-);
+// Orbit camera around center
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 0, 7);
+controls.autoRotate = true;
+controls.autoRotateSpeed = 1;
+controls.update();
 
+/**
+ * Generates cube container
+ */
+function create_cube(side) {
+  let geometry = new THREE.BoxGeometry(side, side, side);
+  let edges = new THREE.EdgesGeometry(geometry);
+  let cube = new THREE.LineSegments(
+    edges,
+    new THREE.LineBasicMaterial({ color: 0xffffff })
+  );
+  return cube;
+}
+
+// Generates snake block
+function create_snake_initial(side) {
+  let geometry = new THREE.BoxGeometry(side, side, side);
+  var material = new THREE.MeshBasicMaterial({ color: 0x8bf0ba });
+  let head = new THREE.Mesh(geometry, material);
+  return head;
+}
+
+// Generates food sphere
+function new_food(radius) {
+  let geometry = new THREE.SphereGeometry(radius);
+  var material = new THREE.MeshBasicMaterial({ color: 0xff214f });
+  let shpere = new THREE.Mesh(geometry, material);
+  return shpere;
+}
+
+// Updates food location randomly
+function change_food_location(food) {
+  const rand_x = Math.random() * side - side / 2;
+  const rand_y = Math.random() * side - side / 2;
+  const rand_z = Math.random() * side - side / 2;
+  food.position.set(rand_x, rand_y, rand_z);
+}
+
+// Create the big cube container
+let cube = create_cube(side);
 scene.add(cube);
 
-camera.position.z = 5;
+// Set snake
+let snake = create_snake_initial(side / 60);
+// scene.add(snake);
+
+// Create food
+let food = new_food(snake_side / 2);
+change_food_location(food);
+scene.add(food);
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.001;
-  cube.rotation.y += 0.001;
+
+  controls.update();
   renderer.render(scene, camera);
 }
 animate();
