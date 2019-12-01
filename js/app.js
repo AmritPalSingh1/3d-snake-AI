195a45af8a7e1c25f18b38af568ee311015c5f3c
asingh369 <amritpalsinghs369@gmail.com>
2019-12-01 03:02:46 -0800

    create transparent cube

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  1,
-  500
+var geometry = new THREE.BoxGeometry(3, 3, 3);
+var edges = new THREE.EdgesGeometry(geometry);
+var cube = new THREE.LineSegments(
+  edges,
+  new THREE.LineBasicMaterial({ color: 0xffffff })
 );
-camera.position.set(0, 0, 100);
-camera.lookAt(0, 0, 0);
 
-var scene = new THREE.Scene();
+scene.add(cube);
 
-var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+camera.position.z = 5;
 
-var geometry = new THREE.Geometry();
-geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
-geometry.vertices.push(new THREE.Vector3(0, 10, 0));
-geometry.vertices.push(new THREE.Vector3(10, 0, 0));
-
-var line = new THREE.Line(geometry, material);
-
-scene.add(line);
-renderer.render(scene, camera);
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.001;
+  cube.rotation.y += 0.001;
+  renderer.render(scene, camera);
+}
+animate();
