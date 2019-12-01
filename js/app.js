2d0c922821ff25a121fabbf86cf22e1181f8bf85
asingh369 <amritpalsinghs369@gmail.com>
2019-12-01 14:49:40 -0800

    Implement queue

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 //  size of cube
 const side = 3;
 // size of snake cube
-const snake_side = side / 40;
+const snake_side = side / 20;
 
 /**
  * Page setup
@@ -18,7 +18,7 @@ var camera = new THREE.PerspectiveCamera(
 );
 camera.position.z = 5;
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -28,6 +28,41 @@ camera.position.set(0, 0, 7);
 controls.autoRotate = true;
 controls.autoRotateSpeed = 1;
 controls.update();
+
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return "Underflow";
+    }
+    return this.items.shift();
+  }
+
+  front() {
+    if (this.isEmpty()) {
+      return "No element in Queue";
+    }
+    return this.items[0];
+  }
+
+  isEmpty() {
+    // return true if the queue is empty.
+    return this.items.length == 0;
+  }
+
+  printQueue() {
+    var str = "";
+    for (var i = 0; i < this.items.length; i++) str += this.items[i] + " ";
+    return str;
+  }
+}
 
 /**
  * Generates cube container
@@ -43,7 +78,7 @@ function create_cube(side) {
 }
 
 // Generates snake block
-function create_snake_initial(side) {
+function create_snake_block(side) {
   let geometry = new THREE.BoxGeometry(side, side, side);
   var material = new THREE.MeshBasicMaterial({ color: 0x8bf0ba });
   let head = new THREE.Mesh(geometry, material);
@@ -53,7 +88,7 @@ function create_snake_initial(side) {
 // Generates food sphere
 function new_food(radius) {
   let geometry = new THREE.SphereGeometry(radius);
-  var material = new THREE.MeshBasicMaterial({ color: 0xff214f });
+  var material = new THREE.MeshBasicMaterial({ color: 0x2fddf4 });
   let shpere = new THREE.Mesh(geometry, material);
   return shpere;
 }
@@ -66,13 +101,15 @@ function change_food_location(food) {
   food.position.set(rand_x, rand_y, rand_z);
 }
 
+function move_forword() {}
+
 // Create the big cube container
 let cube = create_cube(side);
 scene.add(cube);
 
 // Set snake
-let snake = create_snake_initial(side / 60);
-// scene.add(snake);
+let snake = create_snake_block(snake_side);
+scene.add(snake);
 
 // Create food
 let food = new_food(snake_side / 2);
@@ -86,3 +123,11 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+
+function on_window_resize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", on_window_resize, false);
